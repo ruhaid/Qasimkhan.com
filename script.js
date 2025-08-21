d29a4c511e321dcfbc02373f133d569e1ccab784
Qasim Khan <qasimkhan007ak47@gmail.com>
2025-08-21 20:20:15 +0500

    this is my portfolio

---
+++ script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// ------------------project--------------------
+const  projectData = [
+  {name:"news website", img:"news.png",discription:"This is a bueatiful responsive resturent wibsite ",link:"https://ruhaid.github.io/ptmkhyber/"},
+];
+let projectDiv = document.getElementById("project");
+projectData.forEach((porject)=>{
+let projectContainer = document.createElement("div");
+projectContainer.classList.add("project");
+projectContainer.innerHTML=`<img src="${porject.img}" alt="">
+  <h1>${porject.name}</h1>
+  <p>${porject.discription}</p>
+  <a href="${porject.link}"><button>Porject veiw</button></a>
+`;
+  projectDiv.appendChild(projectContainer)
+})
+
